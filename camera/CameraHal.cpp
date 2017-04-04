3c75dc7f4f009e11d4c0962d291f2a4cbd09aefc
Andreas Blaesius <skate4life@gmx.de>
2017-04-04 11:58:41 +0200

    Revert "camera: switch to Gralloc.allocate and generate unique Id for allocation"
    This reverts commit 4a0023edf7dbaa0b3b74e6cf66dd9de7b2421669.
    
    Change-Id: I06382b5147955fe84b332a34ca9e371622f1871a

--- camera/CameraHal.cpp
+++ camera/CameraHal.cpp
@@ -1569,8 +1569,6 @@ status_t CameraHal::allocVideoBufs(uint32_t width, uint32_t height, uint32_t buf
 {
   status_t ret = NO_ERROR;
   LOG_FUNCTION_NAME;
-  static volatile int32_t nextId = 0;
-  uint64_t mId;
 
   if( NULL != mVideoBuffers ){
     ret = freeVideoBufs(mVideoBuffers);
@@ -1591,9 +1589,7 @@ status_t CameraHal::allocVideoBufs(uint32_t width, uint32_t height, uint32_t buf
       for (unsigned int i = 0; i< bufferCount; i++){
         android::GraphicBufferAllocator &GrallocAlloc = android::GraphicBufferAllocator::get();
         buffer_handle_t handle;
-        mId = static_cast<uint64_t>(getpid()) << 32;
-        mId |= static_cast<uint32_t>(android_atomic_inc(&nextId));
-        ret = GrallocAlloc.allocate(width, height, HAL_PIXEL_FORMAT_NV12, CAMHAL_GRALLOC_USAGE, &handle, &stride, mId, "CameraHal");
+        ret = GrallocAlloc.alloc(width, height, HAL_PIXEL_FORMAT_NV12, CAMHAL_GRALLOC_USAGE, &handle, &stride);
         if (ret != NO_ERROR){
           CAMHAL_LOGEA("Couldn't allocate video buffers using Gralloc");
           ret = -NO_MEMORY;
