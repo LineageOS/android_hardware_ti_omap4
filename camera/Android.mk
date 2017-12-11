f5a98d32889a9decfd34f2ec52579a2c01606548
Michael Gernoth <michael@gernoth.net>
2017-12-11 21:21:56 +0100

    Revert "omap4: add local libjhead"
    jhead was forked to LineageOS/android_external_jhead, so use it
    instead of the local copy (by adding a dependency in the device tree).
    
    This reverts commit 0f456ee3a0d8cc79b708a11b505a9e376d467a2b.
    
    Change-Id: I293eaf84104c61860e24f6114cbcddec3cc272fe

--- camera/Android.mk
+++ camera/Android.mk
@@ -90,7 +90,7 @@ TI_CAMERAHAL_COMMON_INCLUDES := \
     $(LOCAL_PATH)/../include \
     $(LOCAL_PATH)/../hwc \
     external/jpeg \
-    $(LOCAL_PATH)/../libjhead \
+    external/jhead \
     $(LOCAL_PATH)/../libtiutils \
     $(LOCAL_PATH)/inc \
     system/media/camera/include
